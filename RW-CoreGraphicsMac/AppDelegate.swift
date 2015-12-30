42dd51ba68f35c38d1bd466c8ec1e11bf68c444f
Eric Elfner <eje@geoderanch.com>
2015-12-30 09:04:35 -0500

    Shared code continued.

---
+++ RW-CoreGraphicsMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RW-CoreGraphicsMac
+//
+//  Created by Eric Elfner on 2015-12-30.
+//  Copyright © 2015 Eric Elfner. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RW-CoreGraphicsMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RW-CoreGraphicsMac
+//
+//  Created by Eric Elfner on 2015-12-30.
+//  Copyright © 2015 Eric Elfner. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
